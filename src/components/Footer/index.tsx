88dc68eb8b6bec132a7bc9d408b07ed1315a3cf3
Julia <jujucc_2003@hotmail.com>
2026-08-03 16:41:44 -0300

    HTML-CSS Header & footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,36 @@
+import "./index.css";
+
+import logo from "../../assets/images/footer/logo-copinha-2026.svg";
+import iconCadeado from "../../assets/images/footer/icon-cadeado.svg";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <img
+          src={logo}
+          alt="Logo CopINha"
+          className="footer-logo"
+        />
+
+        <nav className="footer-nav">
+          <span>Copa do Mundo</span>
+          <span>•</span>
+          <span>Fase de Grupos</span>
+
+          <div className="admin-area">
+            
+            <a href="#" className="admin-link">
+                <img
+                    src={iconCadeado}
+                    alt=""
+                    className="admin-icon"
+                />
+                <span>Área administrativa</span>
+            </a>
+          </div>
+        </nav>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,43 @@
+import "./index.css";
+
+import logo from "../../assets/images/header/logo-copinha.svg";
+import iconNoticias from "../../assets/images/header/icon-noticias.svg";
+import iconGrupos from "../../assets/images/header/icon-grupos.svg";
+import iconJogos from "../../assets/images/header/icon-jogos.svg";
+import iconSimulador from "../../assets/images/header/icon-simulador.svg";
+
+export default function Header() {
+    return (
+        <header className="header">
+
+            <div className="logo">
+                <img src={logo} alt="Logo CopINha" />
+            </div>
+
+            <nav className="menu">
+
+                <a href="#" className="menu-item menu-item-ativo">
+                    <img src={iconNoticias} alt="" className="noticias-icone" />
+                    <span>Notícias</span>
+                </a>
+
+                <a href="#" className="menu-item">
+                    <img src={iconGrupos} alt="" className="grupos-icone" />
+                    <span>Grupos</span>
+                </a>
+
+                <a href="#" className="menu-item">
+                    <img src={iconJogos} alt="" className="jogos-icone" />
+                    <span>Jogos</span>
+                </a>
+
+                <a href="#" className="menu-item">
+                    <img src={iconSimulador} alt="" className="simulador-icone" />
+                    <span>Simulador</span>
+                </a>
+
+            </nav>
+
+        </header>
+    );
+}
