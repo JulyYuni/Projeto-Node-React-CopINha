3c7567b0541d2aa212852b47af3134c2cc2c3632
Caue-Lopes-SI <cauemaior@gmail.com>
2026-08-07 17:04:15 -0300

    add: types

---
+++ src/types/Jogos.ts
@@ -0,0 +1,26 @@
+export type Jogos ={
+    id: string;
+    timeIdCasa: string;
+    timeIdFora: string;
+    status: string;
+    golsCasa: number;
+    golsFora: number;
+    data: string;
+    local: string;
+    createdAt: string;
+    updatedAt: string;
+
+}
+
+
+//     "id": "uuid-do-jogo",
+//     "grupoId": "uuid-do-grupo",
+//     "timeIdCasa": "uuid-do-time-casa",
+//     "timeIdFora": "uuid-do-time-fora",
+//     "status": "ENCERRADO",
+//     "golsCasa": 2,
+//     "golsFora": 1,
+//     "data": "2026-08-10T16:00:00.000Z",
+//     "local": "Estádio Maracanã",
+//     "createdAt": "2026-08-01T12:00:00.000Z",
+//     "updatedAt": "2026-08-10T18:00:00.000Z"
