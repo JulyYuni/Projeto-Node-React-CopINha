f580b6a8fc9023b1b5c9bc354da475b58308b305
Caue-Lopes-SI <cauemaior@gmail.com>
2026-08-07 15:01:19 -0300

    add: types grupos e times

---
+++ src/types/Grupos.ts
@@ -0,0 +1,5 @@
+export type Grupo = {
+    id: string;
+    nome: string;
+    createdAt: string;
+};
---
+++ src/types/Times.ts
@@ -0,0 +1,14 @@
+export type Time = {
+  id: string;
+  nome: string;
+  sigla: string;
+  bandeira: string;
+  grupoId: string;
+  vitorias: number;
+  empate: number;
+  derrotas: number;
+  golsPro: number;
+  golsContra: number;
+  pontos: number;
+  classificado: boolean;
+};
