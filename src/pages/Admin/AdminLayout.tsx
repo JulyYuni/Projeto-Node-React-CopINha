57701fc76571e907db1a8caf50e01b88bdada439
Caue-Lopes-SI <cauemaior@gmail.com>
2026-08-07 14:40:59 -0300

    add: Admin header

--- src/pages/Admin/AdminLayout.tsx
+++ src/pages/Admin/AdminLayout.tsx
@@ -2,12 +2,14 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/SideBar";
 import styles from "./styles.module.css";
+import AdminHeader from "../../components/AdminHeader";
 
 export default function AdminLayout() {
   return (
     <div className={styles.adminWrapper}>
       <Sidebar />
       <main className={styles.content}>
+        <AdminHeader/>
         <Outlet />
       </main>
     </div>
