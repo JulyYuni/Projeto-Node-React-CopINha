bad1e0a6ceb82ecf3b24fcad8896aee596eaa646
Caue-Lopes-SI <cauemaior@gmail.com>
2026-08-07 20:35:21 -0300

    add: login e grupos

--- src/pages/Admin/AdminGroups/index.tsx
+++ src/pages/Admin/AdminGroups/index.tsx
@@ -1,6 +1,44 @@
+import styles from "./styles.module.css";
+import trashIcon from "../../../assets/icons/trash.png";
+import { Grupos as gruposMock } from "../../../assets/Mocks/grupos";
+import { useState } from "react";
 
-export default function Admingroups(){
-    return(
-        <h1>Placeholder AdminGroups</h1>
-    )
+function formatarData(isoString: string) {
+  //essa função vai mudar a data do formato que colocaram na API pra um formato legível em extenso
+  return new Intl.DateTimeFormat("pt-BR", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(isoString));
+}
+
+export default function Admingroups() {
+  const [grupos, setGrupos] = useState(gruposMock);
+  function excluirGrupo(id: string) {
+    setGrupos((atual) => atual.filter((g) => g.id !== id));
+  }
+  return (
+    <div className={styles.groups}>
+      <button className={styles.btn}> + Novo Grupo </button>
+      <div className={styles.sheet}>
+        <div className={styles.sheetHeader}>
+          <span>GRUPO</span>
+          <span>CRIADO EM</span>
+        </div>
+        <div className={styles.sheetGroups}>
+          {grupos.map((grupo) => (
+            <tr key={grupo.id} className={styles.sheetRow}>
+              <td className={styles.groupName}>{grupo.nome}</td>
+              <td className={styles.groupData}>{formatarData(grupo.createdAt)}</td>
+              <td>
+                <button onClick={() => excluirGrupo(grupo.id)}>
+                  <img src={trashIcon} alt="excluir" className={styles.trashIcon}/>
+                </button>
+              </td>
+            </tr>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,44 +1,78 @@
-import styles from "./styles.module.css";
-import Lock from "../../assets/icons/lock.png";
-import Logo from "../../components/Logo";
-import { Link } from "react-router-dom";
+  import styles from "./styles.module.css";
+  import Logo from "../../components/Logo";
+  import { Link } from "react-router-dom";
+  import { useForm } from "react-hook-form";
+  import { z } from "zod";
+  import { zodResolver } from "@hookform/resolvers/zod";
 
-export default function Login() {
-  return (
-    <div className={styles.loginPage}>
-      <header className={styles.header}>
-        <Link to={"/"} className={styles.logo}><Logo/></Link>
-      </header>
-      <div className={styles.loginCard}>
-        <div className={styles.lock}>
-          <img src={Lock} alt="cadeado" />
+  const loginSchema = z.object({
+    email: z.string().email("Email inválido"),
+
+    password: z.string().min(1, "Digite sua senha"),
+  });
+
+  type LoginFormData = z.infer<typeof loginSchema>;
+
+  export default function Login() {
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm<LoginFormData>({
+      resolver: zodResolver(loginSchema),
+    });
+
+    function onSubmit(data: LoginFormData) {
+      console.log(data);
+    }
+
+    return (
+      <div>
+        <Link to="/" className={styles.logo}>
+          <Logo />
+        </Link>
+
+        <div>
+          <h1>Acesso restrito</h1>
+
+          <p>Painel administrativo · Cop{"{IN}"}ha</p>
+
+          <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+            <div className={styles.field}>
+              <label htmlFor="email">E-MAIL</label>
+
+              <input
+                id="email"
+                type="email"
+                placeholder="admin@copinha.com"
+                {...register("email")}
+              />
+
+              {errors.email && <span>{errors.email.message}</span>}
+            </div>
+
+            <div className={styles.field}>
+              <label htmlFor="senha">SENHA</label>
+
+              <input
+                id="senha"
+                type="password"
+                placeholder="**********"
+                {...register("password")}
+              />
+
+              {errors.password && <span>{errors.password.message}</span>}
+            </div>
+
+            <button type="submit" className={styles.submitButton}>
+              Entrar no painel
+            </button>
+          </form>
+
+          <p className={styles.footerText}>
+            Acesso exclusivo para administradores
+          </p>
         </div>
-        <div className={styles.info}>
-          <h1 className={styles.title}>Acesso restrito</h1>
-          <p className={styles.subtitle}>Painel administrativo · Cop{"{IN}"}ha</p>
-        </div>
-
-        <form className={styles.form}>
-          <div className={styles.field}>
-            <label htmlFor="email">E-MAIL</label>
-            <input id="email" type="email" placeholder="admin@copinha.com" />
-          </div>
-
-          <div className={styles.field}>
-            <label htmlFor="senha">SENHA</label>
-            <input id="senha" type="password" placeholder="**********" />
-          </div>
-
-          <button type="submit" className={styles.submitButton}>
-            {/* <img src={Lock} alt="cadeado" /> */}
-            Entrar no painel
-          </button>
-        </form>
-
-        <p className={styles.footerText}>
-          Acesso exclusivo para administradores
-        </p>
       </div>
-    </div>
-  );
-}
+    );
+  }
